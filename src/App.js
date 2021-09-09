7ebf05bb1fa9c9b2000415f075d0a4a34c5e245f
Amit Prakash <amitprakash0111@gmail.com>
2021-09-09 20:27:51 +0530

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import profileImage from './img/myimage.jpeg'
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to Kavita-Kasis </h1>
+      <h2>Amit Prakash</h2>
+      <img src={profileImage} alt = "kavit-kasis" />
+    </div>
+  );
+}
+
+export default App;
